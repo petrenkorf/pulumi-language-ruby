3f8b66ee94e16d0893256d7b4f3f48a4ab7c754b
Petris Fernandes <petrisrf@gmail.com>
2026-02-16 18:32:58 -0300

    Fix path for generated files

--- generate.sh
+++ generate.sh
@@ -3,4 +3,4 @@
 grpc_tools_ruby_protoc --proto_path=. \
   --ruby_out=./lib/ruby_pulumi/generated \
   --grpc_out=./lib/ruby_pulumi/generated \
-  $(find proto -name "*.proto")
+  $(find pulumi -name "*.proto")
